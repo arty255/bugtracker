df685431842a05a25c28f2cd993c2ceb472e2166
artya <artyagent@gmail.com>
2020-12-23 20:28:11 +0300

    Separate configs created. Redundant bean definition in AppConfig replaced to @service and @registry annotations, component scan updated.

--- src/main/java/org/hetsold/bugtracker/WebAppConfig.java
+++ src/main/java/org/hetsold/bugtracker/WebAppConfig.java
@@ -1,9 +1,5 @@
 package org.hetsold.bugtracker;
 
-<<<<<<< HEAD
-import org.springframework.context.annotation.ComponentScan;
-=======
->>>>>>> 81dc8e6... Separate configs created. Redundant bean definition in AppConfig replaced to @service and @registry annotations, component scan updated.
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
@@ -13,7 +9,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 @Configuration()
 @Import(AppConfig.class)
-//@ComponentScan(basePackages = {"org.hetsold.bugtracker.rest"})
 public class WebAppConfig implements WebMvcConfigurer {
 
     @Override
