8acb4b888c4a1c9886fbe510e2c3bfa77f7095b6
artya <artyagent@gmail.com>
2020-12-30 22:47:59 +0300

    Minor TicketService changes

--- src/main/java/org/hetsold/bugtracker/service/DefaultUserService.java
+++ src/main/java/org/hetsold/bugtracker/service/DefaultUserService.java
@@ -9,8 +9,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
-
 @Service
 @Transactional
 public class DefaultUserService implements UserService {
