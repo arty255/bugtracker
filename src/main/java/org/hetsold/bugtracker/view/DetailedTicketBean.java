f9586c72857b60918d8525bd814f6e33c979831a
artya <artyagent@gmail.com>
2021-01-05 08:42:14 +0300

    T-JSF-07. Implement possibility to edit ticket in detailed ticket view. updateTicket method added to TicketService.

--- src/main/java/org/hetsold/bugtracker/view/DetailedTicketBean.java
+++ src/main/java/org/hetsold/bugtracker/view/DetailedTicketBean.java
@@ -6,16 +6,15 @@ import org.hetsold.bugtracker.model.UserDTO;
 import org.hetsold.bugtracker.service.MessageService;
 import org.hetsold.bugtracker.service.TicketService;
 import org.hetsold.bugtracker.service.UserService;
-import org.primefaces.component.tabview.Tab;
-import org.primefaces.event.TabChangeEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.jsf.FacesContextUtils;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
-import javax.faces.event.ActionEvent;
+import javax.faces.event.ValueChangeEvent;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,9 +24,7 @@ import java.util.List;
 public class DetailedTicketBean implements Serializable {
     private String uuid;
     private TicketDTO selectedTicketDTO;
-
     private List<MessageDTO> ticketMessages;
-
     private MessageDTO selectedMessage;
 
     @Autowired
@@ -69,10 +66,19 @@ public class DetailedTicketBean implements Serializable {
 
     }
 
+    public void updateTicket() {
+        /*todo: getUser FromSecurityContext*/
+        UserDTO user = userService.getUserById("19f0a834-4324-419a-8828-50494c2353e4");
+        ticketService.updateTicket(selectedTicketDTO, user);
+    }
+
+    public void tickedChangedListener(ValueChangeEvent event) {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Warning!", "Ticked changed. Dont forget save."));
+    }
+
     public void addMessageToTicket() {
         /*todo: getUser FromSecurityContext*/
         UserDTO user = userService.getUserById("19f0a834-4324-419a-8828-50494c2353e4");
-        //ticketService.addTicketMessage(selectedTicketDTO, new MessageDTO(messageContent), user);
         ticketService.addTicketMessage(selectedTicketDTO, selectedMessage, user);
         initMessage();
         initMessageList();
@@ -90,7 +96,6 @@ public class DetailedTicketBean implements Serializable {
 
     public void initMessage() {
         selectedMessage = new MessageDTO("");
-        //messageContent = "";
     }
 
     public List<MessageDTO> getTicketMessages() {
