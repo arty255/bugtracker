7649ab05e1fc2477e61ac718ec3464e5613eaece
artya <artyagent@gmail.com>
2021-01-05 08:40:17 +0300

    TicketService interface changes. Imports optimisation.

--- src/test/java/org/hetsold/bugtracker/TestAppConfig.java
+++ src/test/java/org/hetsold/bugtracker/TestAppConfig.java
@@ -1,7 +1,5 @@
 package org.hetsold.bugtracker;
 
-import org.hetsold.bugtracker.facade.IssueFacade;
-import org.mockito.Mockito;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
