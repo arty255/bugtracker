f940fd12b74651dfa962d2ea8d5e4194bfc9cc50
artya <artyagent@gmail.com>
2021-01-31 17:05:13 +0300

    T-Core-007. Refactor service layer. UserService changes: getUsers and getUsersCount now work with Contract, delete now work with uuid; save, listAllUsers methods deleted. According changes in DAO. UserConvertor renamed to UserMapper, list mapping method added.

--- src/main/java/org/hetsold/bugtracker/dao/UserDAO.java
+++ src/main/java/org/hetsold/bugtracker/dao/UserDAO.java
@@ -1,19 +1,18 @@
 package org.hetsold.bugtracker.dao;
 
 import org.hetsold.bugtracker.model.User;
+import org.hetsold.bugtracker.model.filter.Contract;
 
 import java.util.List;
 
 public interface UserDAO {
     void save(User user);
 
-    List<User> listAll();
-
     void delete(User user);
 
     User getUserById(String uuid);
 
-    long getUsersCount();
+    long getUsersCount(Contract contract);
 
-    List<User> getUsers(int first, int count);
+    List<User> getUsers(Contract contract, int first, int limit);
 }
--- src/main/java/org/hetsold/bugtracker/service/UserService.java
+++ src/main/java/org/hetsold/bugtracker/service/UserService.java
@@ -2,27 +2,25 @@ package org.hetsold.bugtracker.service;
 
 import org.hetsold.bugtracker.model.User;
 import org.hetsold.bugtracker.model.UserDTO;
+import org.hetsold.bugtracker.model.filter.Contract;
 
 import java.util.List;
 
 public interface UserService {
-    User save(User user);
 
     UserDTO registerUser(UserDTO userDTO);
 
-    void updateUser(UserDTO userDTO);
+    UserDTO updateUser(UserDTO userDTO);
 
     User getUserById(String id);
 
     UserDTO getUserDTOById(String id);
 
-    void delete(User user);
+    void delete(String uuid);
 
     void delete(UserDTO userDTO);
 
-    List<UserDTO> getAllUsers();
+    List<UserDTO> getUsers(Contract contract, int first, int count);
 
-    List<UserDTO> getUsers(int first, int count);
-
-    long getUserCount();
+    long getUsersCount(Contract contract);
 }
